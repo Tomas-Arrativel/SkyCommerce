e8a236772c111e595b0ae7ccb355daf69905d3cd
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-08 11:39:50 -0300

    Navbar for desktop finished

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,19 @@
 import './Navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='nav'>
+      <h1 className='nav__title'>
+        Sky<span>Commerce</span>
+      </h1>
+      <input
+        className='nav__search'
+        type='text'
+        placeholder='Search in SkyCommerce'
+      />
+      <p className='nav__chart'>Chart</p>
+    </nav>
+  );
 };
 
 export default Navbar;
