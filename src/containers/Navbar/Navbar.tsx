9c99eed81af75e5c32129b7ebd07dc815acbaf82
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-09 09:31:00 -0300

    made the Search responsive in the navbar

--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Search, ShoppingCart } from '../../components/exports';
 
 import './Navbar.css';
-
 const Navbar = () => {
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // eventListener to window resize
+    window.addEventListener('resize', handleResize);
+
+    // clean eventListener when desmounting
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [screenWidth]); // is called every time screenWidth changes
+
+  const isMobile = screenWidth <= 735;
+
   return (
-    <nav className='nav'>
-      <h1 className='nav__title'>
-        Sky<span>Commerce</span>
-      </h1>
-      <Search />
-      <ShoppingCart />
-    </nav>
+    <div className='nav-container'>
+      <nav className='nav'>
+        <h1 className='nav__title'>
+          Sky<span>Commerce</span>
+        </h1>
+        {!isMobile ? <Search /> : null}
+        <ShoppingCart />
+      </nav>
+      {isMobile ? <Search /> : null}
+    </div>
   );
 };
 
