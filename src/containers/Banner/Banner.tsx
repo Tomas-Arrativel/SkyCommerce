4b15fb240aa5e781b3f1494a99f1c732d5fea508
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-10 10:21:42 -0300

    Adding images to offer categories

--- src/containers/Banner/Banner.tsx
+++ src/containers/Banner/Banner.tsx
@@ -11,8 +11,16 @@ const Banner = () => {
           title='Deals in technology'
           img='/offers/iPhone-9.webp'
         />
-        <Offer btnText='Shop now' title='Products under $25' img='' />
-        <Offer btnText='See more' title='Refresh your space' img='' />
+        <Offer
+          btnText='Shop now'
+          title='Products under $25'
+          img='/offers/perfume-oil.webp'
+        />
+        <Offer
+          btnText='See more'
+          title='Refresh your space'
+          img='/offers/decoration.webp'
+        />
       </div>
     </section>
   );
