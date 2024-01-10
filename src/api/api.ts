b9262b908e0f57fb6946e345450c4b71deb08801
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-10 09:16:08 -0300

    Made the Api reusable function fetchData

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://dummyjson.com/products';
+
+const fetchData = async (endpoints: string = '') => {
+  const url = endpoints ? `${BASE_URL}/${endpoints}` : BASE_URL;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || 'Error en la solicitud');
+  }
+
+  return data;
+};
+
+export { fetchData };
