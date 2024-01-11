31436bb9f9edfbbf03160d05c294872c5af4a354
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-11 09:44:43 -0300

    creating the container of the carousels

---
+++ src/containers/SomeProducts/SomeProducts.tsx
@@ -0,0 +1,7 @@
+import './SomeProducts.css';
+
+const SomeProducts = () => {
+  return <div>SomeProducts</div>;
+};
+
+export default SomeProducts;
--- src/containers/exports.ts
+++ src/containers/exports.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Categories } from './Categories/Categories';
 export { default as Banner } from './Banner/Banner';
+export { default as SomeProducts } from './SomeProducts/SomeProducts';
