3982d476f6c5a498095d340c73b8cf130f409b2f
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-15 18:37:05 -0300

    Adding the decoration category

--- src/containers/SomeProducts/SomeProducts.tsx
+++ src/containers/SomeProducts/SomeProducts.tsx
@@ -1,4 +1,4 @@
-import { Carousel } from '../../components/exports';
+import { SeeMore } from '../../components/exports';
 
 import './SomeProducts.css';
 
@@ -7,7 +7,14 @@ const SomeProducts = () => {
     <div className='someprods'>
       <div className='carousel__container'>
         <h2>The best on Technology</h2>
-        <Carousel categorie='Technology' />
+        <SeeMore categorie='Technology' />
+        <a href='#'>See More</a>
+      </div>
+
+      <div className='carousel__container'>
+        <h2>The best on Decoration</h2>
+        <SeeMore categorie='Decoration' />
+        <a href='#'>See More</a>
       </div>
     </div>
   );
