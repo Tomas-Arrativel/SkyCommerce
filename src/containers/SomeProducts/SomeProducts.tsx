e37fe824f315aae63cbcbaf64be8e1934702e53e
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-15 18:40:44 -0300

    styling link in each carousel container

--- src/containers/SomeProducts/SomeProducts.tsx
+++ src/containers/SomeProducts/SomeProducts.tsx
@@ -8,13 +8,17 @@ const SomeProducts = () => {
       <div className='carousel__container'>
         <h2>The best on Technology</h2>
         <SeeMore categorie='Technology' />
-        <a href='#'>See More</a>
+        <a href='#' className='container__link'>
+          See More
+        </a>
       </div>
 
       <div className='carousel__container'>
         <h2>The best on Decoration</h2>
         <SeeMore categorie='Decoration' />
-        <a href='#'>See More</a>
+        <a href='#' className='container__link'>
+          See More
+        </a>
       </div>
     </div>
   );
