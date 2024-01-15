6efc8575a28d32e80b0bb017aea3ef54d08cf0d9
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-15 18:36:50 -0300

    SeeMore Component finished

---
+++ src/components/SeeMore/SeeMore.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { fetchData } from '../../api/api';
+
+import './SeeMore.css';
+
+interface CarouselProps {
+  categorie: string;
+}
+
+// structure of a product
+interface Product {
+  id: number;
+  images: string[];
+  title: string;
+}
+
+const SeeMore = ({ categorie }: CarouselProps) => {
+  const [data, setData] = useState<any>();
+
+  // Endpoint for api call
+  let endpoint: string;
+  if (categorie === 'Technology') endpoint = 'category/smartphones';
+  else if (categorie === 'Decoration') endpoint = 'category/home-decoration';
+
+  // api call
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const result = await fetchData(endpoint);
+        setData(result);
+      } catch (error: any) {
+        console.error('Error while obtaining data:', error.message);
+      }
+    };
+    fetchDataAsync();
+  }, []);
+
+  // Container of products
+  return (
+    <div className='products'>
+      {data &&
+        data.products.map((product: Product) => (
+          <a href='#' key={product.id} className='products__card'>
+            <img
+              src={product.images[0]}
+              alt={`${product.title} ${product.id}`}
+            />
+          </a>
+        ))}
+    </div>
+  );
+};
+
+export default SeeMore;
