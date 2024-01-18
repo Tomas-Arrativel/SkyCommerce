b987b34c7af0c9bf544f8934b3136f89525f1277
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-18 11:02:39 -0300

    making the Search input search onkey down and clicking the button

--- src/components/Navbar/Search/Search.tsx
+++ src/components/Navbar/Search/Search.tsx
@@ -1,16 +1,38 @@
 import { FaSearch } from 'react-icons/fa';
 
 import './Search.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+  const [typed, setTyped] = useState<any>();
+  const navigate = useNavigate();
+
+  const handleInputChange = (e: any) => {
+    setTyped(e.target.value);
+  };
+
+  const handleEnterPress = (e: any) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const handleSearch = () => {
+    // redirect to SearchPage with the query
+    navigate(`/search/${typed.toLowerCase()}`);
+  };
+
   return (
     <div className='search'>
       <input
         className='search__input'
         type='text'
         placeholder='Search in SkyCommerce'
+        onChange={(e) => handleInputChange(e)}
+        onKeyDown={(e) => handleEnterPress(e)}
       />
-      <button className='search__button'>
+      <button onClick={handleSearch} className='search__button'>
         <FaSearch />
       </button>
     </div>
