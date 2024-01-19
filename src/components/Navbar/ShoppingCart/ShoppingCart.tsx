8f67e5c7b4c6c62c6919516bf00aa75bf52f67d1
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-19 10:58:58 -0300

    added quantity from shoppingCartContext()

--- src/components/Navbar/ShoppingCart/ShoppingCart.tsx
+++ src/components/Navbar/ShoppingCart/ShoppingCart.tsx
@@ -1,12 +1,14 @@
 import { FaShoppingCart } from 'react-icons/fa';
 
 import './ShoppingCart.css';
+import { useShoppingContext } from '../../../context/ShoppingCartContext';
 
 const ShoppingCart = () => {
+  const { cartQuantity } = useShoppingContext();
   return (
     <div className='cart'>
       <FaShoppingCart className='cart__cart' />
-      <p className='cart__items'>0</p>
+      <p className='cart__items'>{cartQuantity}</p>
     </div>
   );
 };
