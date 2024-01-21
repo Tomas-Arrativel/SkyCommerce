42b41a9f53ff98d5099f8ac44fddf19e842fac48
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-21 11:45:57 -0300

    Trying to make the Total price component

---
+++ src/components/TotalPrice/TotalPrice.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useShoppingContext } from '../../context/ShoppingCartContext';
+import { discountPrice } from '../../utilities/discountedPrice';
+import './TotalPrice.css';
+
+interface TotalPriceProps {
+  id: number;
+  price: number;
+  discountPercentage: number;
+}
+
+const TotalPrice = ({ items }: any) => {
+  const { getItemQuantity } = useShoppingContext();
+  let finalPrice: number = 0;
+
+  items.map((item: TotalPriceProps) => {
+    const quantity = getItemQuantity(item.id);
+    const discountedPrice: any = discountPrice(
+      item.price,
+      item.discountPercentage,
+    );
+    finalPrice += quantity * discountedPrice;
+  });
+  console.log(finalPrice);
+
+  return <h2 className='final__price'>Total: {finalPrice}</h2>;
+};
+
+export default TotalPrice;
--- src/components/exports.ts
+++ src/components/exports.ts
@@ -4,3 +4,4 @@ export { default as Offer } from './Offer/Offer';
 export { default as SeeMore } from './SeeMore/SeeMore';
 export { default as Product } from './Product/Product';
 export { default as CartItem } from './CartItem/CartItem';
+export { default as TotalPrice } from './TotalPrice/TotalPrice';
